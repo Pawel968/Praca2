9316cb8f08e29ff7060bd33260bd1467fbb18efe
Paweł <cabanpawel96@gmail.com>
2025-04-10 17:51:58 +0200

    praca na lekcji

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,20 @@ import './App.css'
 
 function App() {
   
+const num = 8;
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
   return (
     <>
-      Hello, world!
+      <p>
+        {arr.map((liczba) => {
+          return (
+            <div>
+              {liczba} * {num} = {num * liczba}
+            </div>
+          );
+        })}
+      </p>
     </>
   )
 }
